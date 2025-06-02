d0244216d65c0c5271f0b5e899f02ad363c3012e
Gideon <gideonkimaiyo254@gmail.com>
2025-06-02 16:34:02 +0300

    manupilates the DOM

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'He won he is the champion'; 
+
+document.body.append(newHeader);
