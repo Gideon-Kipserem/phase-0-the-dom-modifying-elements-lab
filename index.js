b7567afd38e3186ad7faaa66c824f14aab4a588f
Gideon <gideonkimaiyo254@gmail.com>
2025-06-28 08:41:10 +0300

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-
-
 const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.textContent = 'He won he is the champion'; 
+newHeader.textContent = 'Gideon Kipserem is the champion';
 
 document.body.append(newHeader);
